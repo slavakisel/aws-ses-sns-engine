0c55ee105240bef1e16eb9d3c42f884bfa051fae
Szymon Kurcab <szymon.kurcab@gmail.com>
2018-10-26 15:46:41 +0200

    Updates

--- lib/aws_ses_sns_engine.rb
+++ lib/aws_ses_sns_engine.rb
@@ -6,6 +6,9 @@ require "aws_ses_sns_engine/sns_subscription_confirmation"
 require 'hashie'
 require 'aws/ses'
 require_relative '../app/models/aws_ses_sns_engine/ses_sender_email.rb'
+require_relative '..app/controllers/aws_ses_sns_engine/application_controller.rb'
+require_relative '../app/controllers/aws_ses_sns_engine/sns_controller.rb'
+
 
 module AwsSesSnsEngine
 end
