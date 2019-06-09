045f8e4a0ca7ef1164b6c53c6ad09fd33fbb1b1d
Slava Kisel <slava@skdev.info>
2019-06-09 17:33:58 +0300

    Construct correct fog settings hash

--- lib/aws_ses_sns_engine/sns_subscription_confirmation.rb
+++ lib/aws_ses_sns_engine/sns_subscription_confirmation.rb
@@ -1,7 +1,10 @@
 module AwsSesSnsEngine
   class SnsSubscriptionConfirmation
     def self.confirm(arn, token)
-      sns = Fog::AWS::SNS.new(SesManager.settings)
+      sns = Fog::AWS::SNS.new(
+      	aws_access_key_id: SesManager.settings[:access_key_id],
+      	aws_secret_access_key: SesManager.settings[:secret_access_key]
+      )
       sns.confirm_subscription(arn, token)
     end
   end
